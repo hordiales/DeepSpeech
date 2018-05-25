37d12ef21705fc1d1c94f3aaf4ac4719e65a2115
Reuben Morais <reuben.morais@gmail.com>
2018-05-25 19:12:01 -0300

    Clean up evaluate.py and add code to save decoded samples to a file

--- evaluate.py
+++ evaluate.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import, division, print_function
 
 import itertools
+import json
 import numpy as np
 import os
 import pandas
@@ -15,7 +16,6 @@ from attrdict import AttrDict
 from collections import namedtuple
 from DeepSpeech import initialize_globals, create_flags, log_debug, log_info, log_warn, log_error, create_inference_graph, decode_with_lm
 from multiprocessing import Pool
-from operator import attrgetter
 from six.moves import zip, range
 from util.audio import audiofile_to_input_vector
 from util.text import sparse_tensor_value_to_texts, text_to_char_array, Alphabet, ctc_label_dense_to_sparse, wer, levenshtein
@@ -29,8 +29,9 @@ N_CONTEXT = 9
 
 
 def pmap(fun, iterable, threads=8):
-    with Pool(threads) as pool:
-        results = pool.map(fun, iterable)
+    pool = Pool(threads)
+    results = pool.map(fun, iterable)
+    pool.close()
     return results
 
 
@@ -149,22 +150,16 @@ def calculate_report(labels, decodings, distances, losses):
     '''
     samples = pmap(process_decode_result, zip(labels, decodings, distances, losses))
 
-    total_levenshtein = sum(map(attrgetter('levenshtein'), samples))
-    total_label_length = sum(map(attrgetter('label_length'), samples))
+    total_levenshtein = sum(s.levenshtein for s in samples)
+    total_label_length = sum(s.label_length for s in samples)
 
     # Getting the WER from the accumulated levenshteins and lengths
     samples_wer = total_levenshtein / total_label_length
 
-    # Filter out all items with WER=0
-    samples = [s for s in samples if s.wer > 0]
-
     # Order the remaining items by their loss (lowest loss on top)
     samples.sort(key=lambda s: s.loss)
 
-    # Take only the first report_count items
-    samples = samples[:FLAGS.report_count]
-
-    # Order this top FLAGS.report_count items by their WER (lowest WER on top)
+    # Then order by WER (lowest WER on top)
     samples.sort(key=lambda s: s.wer)
 
     return samples_wer, samples
@@ -212,8 +207,7 @@ def main(_):
         distance = tf.edit_distance(tf.cast(decoded[0], tf.int32), sparse_labels)
 
         # Create a saver using variables from the above newly created graph
-        mapping = {v.op.name: v for v in tf.global_variables(
-        ) if not v.op.name.startswith('previous_state_')}
+        mapping = {v.op.name: v for v in tf.global_variables() if not v.op.name.startswith('previous_state_')}
         saver = tf.train.Saver(mapping)
 
         # Restore variables from training checkpoint
@@ -239,7 +233,7 @@ def main(_):
 
             batch_features = pad_to_dense(batch['features'].values)
             batch_features_len = batch['features_len'].values
-            full_step_len = np.full_like(batch_features_len, 16)
+            full_step_len = np.full_like(batch_features_len, N_STEPS)
 
             logits = np.empty([0, FLAGS.test_batch_size, alphabet.size() + 1])
             for i in range(0, batch_features.shape[1], N_STEPS):
@@ -266,8 +260,7 @@ def main(_):
                 # we have processed N_STEPS so subtract from remaining steps
                 batch_features_len -= N_STEPS
                 # clip to zero
-                batch_features_len = np.maximum(
-                    batch_features_len, np.zeros_like(batch_features_len))
+                batch_features_len = np.maximum(batch_features_len, np.zeros_like(batch_features_len))
 
             logitses.append(logits)
 
@@ -300,31 +293,26 @@ def main(_):
     mean_edit_distance = np.mean(distances)
     mean_loss = np.mean(losses)
 
-    # Order samples by their loss (lowest loss on top)
-    samples.sort(key=lambda s: s.loss)
-
-    # Take only the first report_count items
-    samples = samples[:FLAGS.report_count]
-
-    # Order this top FLAGS.report_count items by their WER (lowest WER on top)
-    samples.sort(key=lambda s: s.wer)
+    # Filter out all items with WER=0 and take only the first report_count items
+    report_samples = itertools.islice((s for s in samples if s.wer > 0), FLAGS.report_count)
 
     print('Test - WER: %f, loss: %f, mean edit distance: %f' %
           (wer, mean_loss, mean_edit_distance))
-    if len(samples) > 0:
+    if len(report_samples) > 0:
         print('-' * 80)
-        for sample in samples:
+        for sample in report_samples:
             print('WER: %f, loss: %f, mean edit distance: %f' %
                   (sample.wer, sample.loss, sample.distance))
             print(' - src: "%s"' % sample.src)
             print(' - res: "%s"' % sample.res)
             print('-' * 80)
 
+    if FLAGS.test_output_file:
+        json.dump(samples, open(FLAGS.test_output_file, 'w'), default=lambda x: float(x))
+
 
 if __name__ == '__main__':
     create_flags()
-    tf.app.flags.DEFINE_string(
-        'hdf5_test_set',
-        '',
-        'path to hdf5 file to cache test set features')
+    tf.app.flags.DEFINE_string('hdf5_test_set', '', 'path to hdf5 file to cache test set features')
+    tf.app.flags.DEFINE_string('test_output_file', '', 'path to a file to save all src/decoded/distance/loss tuples')
     tf.app.run(main)
